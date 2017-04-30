441c2f1083950699196ff75a5cb6508f03acbb27
RogerZZZZZ <501841352@qq.com>
2017-04-30 18:30:10 +0800

    add server

--- result.js
+++ result.js
@@ -2,10 +2,6 @@ var mysql = require('mysql');
 var moment = require('moment');
 var fs = require('fs');
 var outliers = require('outliers')
-// var MyStream = require('json2csv-stream');
-
-// create the transform stream
-// var parser = new MyStream();
 
 // var connection = mysql.createConnection({
 //   host     : 'www.jlzhang.cn',
@@ -14,138 +10,167 @@ var outliers = require('outliers')
 //   database : 'big_data_test'
 // });
 
-var connection = mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'bigdata'});
+var debug = false;
 
-connection.connect();
+let cityDistrict = [
+    [1, 16],//beijing
+    [17, 31],//shanghai
+    [32, 41],//guangzhou
+    [42, 49]//shenzhen
+];
 
-let testData = {
-    area: 2,
-    // ppty: 4,
-    // status: 1,
-    // owner: 1
-}
+let houseType = [
+    [2, 4, 13, 15, 21],//residential building
+    [3, 7],//villa
+    [6, 12, 14, 16, 19, 20],//shop
+    [18],//office building
+    [5, 8, 9, 10, 11, 17]//apartment
+]
 
-let query = 'SELECT Date, House_id, Price from combine where (`Area_type` = 1';
-for(let i = 2; i <= 16; i++){
-    query += ' OR `Area_type` = ' + i;
-}
-
-query += ') And (`PPTY_Type` = 2 OR `PPTY_Type` = 4 OR `PPTY_Type` = 13)';
-
-// let query = 'SELECT Date, House_id, Price from combine where `Area_type` = ?';
-
-let [q,
-    p] = filter(testData, query);
 let startTime = new Date();
 
-console.log(q, p);
-
-// connection.query(q + ' ORDER BY Date', p, function(error, results, fields) {
-    connection.query(q + ' ORDER BY Date', function(error, results, fields) {
-    if (error)
-        throw error;
-
-    // console.log('The solution is: ', results);
-    // dateTrans('2014-1-1', results[0].DATA_DATE);
-    console.log(results.length);
-    results = results.filter(outliers('Price'));
-    console.log(results.length);
-    let len = results.length;
-    let beginTime = new Date('2013-12-31');
-    let queryEndTime = new Date();
-    console.log('Time spending on querying:' + (queryEndTime - startTime) / (1000) + 'seconds');
-
-    //calculate the max time difference
-    let date1 = new Date(results[0].Date),
-        date2 = new Date(results[1].Date);
-    let max = date2 - date1;
-    let tmpdt1 = results[0],
-        tmpdt2 = results[1];
-    for (let i = 2; i < len; i++) {
-        date1 = new Date(results[i - 1].Date);
-        date2 = new Date(results[i].Date);
-        let tmp = date2 - date1;
-        if (max < tmp) {
-            max = tmp;
-            tmpdt1 = results[i - 1];
-            tmpdt2 = results[i];
-        }
-        //   max = max < tmp ? tmp: max;
+let dataClean = (connection, req, res, next) => {
+    console.log(++global.count);
+    let query = req.query,
+        outlier = query.outlier,
+        area = query.area,
+        ppty = query.ppty,
+        period = query.period;
+    let data = {
+        outlier: outlier,
+        area: area,
+        ppty: ppty,
+        period: period
     }
-    // console.log(max);
-    console.log(max / (1000 * 3600 * 24));
-    let endTime = new Date();
-    console.log(tmpdt1);
-    console.log(tmpdt2);
-    console.log("------------------");
+    innerDataClean(connection, data, (data) => {res.json(data)});
+}
 
-    let dataIndex = 0;
-    let period = 5;
-    let resLen = Math.ceil(3*365 / period);
-    let res = [];
+let innerDataClean = (connection, data, returnFunc) => {
 
-    for (let i = 1; i < resLen; i++){
-        let tmp = 0;
-        let count = 0;
-        let et = moment(beginTime).add(i*period, 'days');
-        for(let j = dataIndex; j < len; j++){
-            let tmpData = results[j];
-            if(moment(tmpData.Date).isBefore(et)){
-                count++;
-                tmp += tmpData.Price;
-            }else{
-                break;
-            }
+    let query = 'SELECT Date, House_id, Price from combine where' ;
+    let [q, p] = filter(data, query);
+    log(q);
+    log(p);
+
+    connection.query(q + ' ORDER BY Date', p, function(error, results, fields) {
+        if (error)
+            throw error;
+
+        if(data.outlier){
+            results = results.filter(outliers('Price'));
         }
-        let tmpRes = count !== 0 ? tmp/count: 0;
-        res.push(tmpRes);
-        dataIndex += count;
-    }
+        log(results.length);
+        let len = results.length;
+        let beginTime = new Date('2013-12-31');
+        let queryEndTime = new Date();
+        log('Time spending on querying:' + (queryEndTime - startTime) / (1000) + 'seconds');
 
-    for(let i = 0; i < resLen; i++){
-        if(res[i] === 0){
-            if(i === 0){
-                let t = i;
-                while(res[++t] === 0);
-                res[0] = res[t];
-            }else{
-                let after = i;
-                let c = 1;
-                while(res[++after] === 0){
-                    c++;
-                }
-                if(after >= resLen - 1){
-                    res[after] = res[i-1];
-                }
-                let k = (res[after]-res[i-1])/c;
-                for(let j = 0; j < c; j++){
-                    res[i+j] = res[i-1] + j * k;
+        //calculate the max time difference
+        if(debug){
+            let date1 = new Date(results[0].Date),
+                date2 = new Date(results[1].Date);
+            let max = date2 - date1;
+            let tmpdt1 = results[0],
+                tmpdt2 = results[1];
+            for (let i = 2; i < len; i++) {
+                date1 = new Date(results[i - 1].Date);
+                date2 = new Date(results[i].Date);
+                let tmp = date2 - date1;
+                if (max < tmp) {
+                    max = tmp;
+                    tmpdt1 = results[i - 1];
+                    tmpdt2 = results[i];
                 }
             }
+            log(max / (1000 * 3600 * 24));
+            let endTime = new Date();
+            log(tmpdt1);
+            log(tmpdt2);
+            log("------------------");
         }
-    }
 
-    console.log(res);
-    console.log(resLen);
+        let dataIndex = 0;
+        let period = parseInt(data.period);
+        let resLen = Math.ceil(3*365 / period);
+        let res = [];
 
-    let writeStr = '';
-    for(let item in res){
-        writeStr += res[item];
-        if(item !== resLen - 1){
-            writeStr += '\n';
+        for (let i = 1; i < resLen; i++){
+            let tmp = 0;
+            let count = 0;
+            let et = moment(beginTime).add(i*period, 'days');
+            for(let j = dataIndex; j < len; j++){
+                let tmpData = results[j];
+                if(moment(tmpData.Date).isBefore(et)){
+                    count++;
+                    tmp += tmpData.Price;
+                }else{
+                    break;
+                }
+            }
+            let tmpRes = count !== 0 ? tmp/count: 0;
+            res.push(tmpRes);
+            dataIndex += count;
         }
-    }
 
-    fs.writeFile("./out.csv", writeStr, function(err) {
-        if(err) {
-            return console.log(err);
+        let lastDataIndex;
+
+        for(let i = 0; i < resLen; i++){
+            if(res[i] === 0){
+                if(i === 0){
+                    let t = i;
+                    while(res[++t] === 0);
+                    res[0] = res[t];
+                }else{
+                    let after = i;
+                    let c = 1;
+                    while(res[++after] === 0){
+                        c++;
+                    }
+                    if(after >= resLen - 1){
+                        // res[after] = res[i-1];
+                        lastDataIndex = i;
+                        lastDate = moment(beginTime).add(i*period, 'days');
+                        break;
+                    }
+                    let k = (res[after]-res[i-1])/c;
+                    for(let j = 0; j < c; j++){
+                        res[i+j] = res[i-1] + j * k;
+                    }
+                }
+            }
         }
-        console.log('Time spending on handling data:' + (endTime - queryEndTime) / (1000) + 'seconds');
+
+        res = res.splice(0,lastDataIndex)
+        log(res);
+        log(resLen);
+        let jsonResult = {
+            data: res,
+            endTime: lastDate
+        };
+        returnFunc(jsonResult);
+
+        //write to the file
+        // let writeStr = '';
+        // for(let item in res){
+        //     writeStr += res[item];
+        //     if(item !== resLen - 1){
+        //         writeStr += '\n';
+        //     }
+        // }
+        //
+        // fs.writeFile("./out.csv", writeStr, function(err) {
+        //     if(err) {
+        //         return log(err);
+        //     }
+        //     console.log('Time spending on handling data:' + (endTime - queryEndTime) / (1000) + 'seconds');
+        // });
     });
 
-});
+}
 
-connection.end();
+function log(obj){
+    if(debug) console.log(obj);
+}
 
 function dateTrans(startTime, target) {
     startTime = new Date(startTime);
@@ -155,10 +180,36 @@ function dateTrans(startTime, target) {
 }
 
 function filter(data, query) {
-    let para = [data.area];
+    let para = [];
+    if(data.area !== undefined){
+        if(data.all){
+            let startIndex =  cityDistrict[data.city][0];
+            let endIndex = cityDistrict[data.city][1];
+            for(let i = startIndex; i <= endIndex; i++){
+                if(i === startIndex){
+                    query += ' (`Area_type` = ?'
+                }else{
+                    query += ' OR `Area_type` = ?';
+                }
+                para.push(i);
+            }
+            query += ')'
+        }else{
+            query += ' `Area_type` = ?';
+            para.push(data.area);
+        }
+    }
     if (data.ppty !== undefined) {
-        query += ' And `PPTY_type` = ?';
-        para.push(data.ppty);
+        let type = houseType[data.ppty];
+        for(let i = 0; i < type.length; i++){
+            if(i === 0){
+                query += ' And ( `PPTY_type` = ?';
+            }else{
+                query += ' OR `PPTY_type` = ?';
+            }
+            para.push(type[i])
+        }
+        query += ')'
     }
     if (data.status !== undefined) {
         query += ' And `STATUS_TYPE` = ?';
@@ -171,3 +222,5 @@ function filter(data, query) {
 
     return [query, para];
 }
+
+exports.dataClean = dataClean;
--- server.js
+++ server.js
@@ -1,80 +1,35 @@
-var mysql = require('mysql');
-var fs = require('fs');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    compression = require('compression'),
+    data = require('./result'),
+    app = express();
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'bigdata'
-});
-var startTime = new Date();
-let minuteUnit = 1000;
+app.set('port', process.env.PORT || 5000);
+global.count = 0;
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({host: 'localhost', user: 'root', password: '', database: 'bigdata'});
 connection.connect();
 
-connection.query("SELECT info.HOUSE_SRN, A.Id as Area_type, type.id as PPTY_TYPE, s.id as STATUS_TYPE, info.OWNERSHIP_PERIOD as Ownership from House_info_data as info, House_status as s, house_type as type, Area_type as A where info.PPTY_MGMT_TYPE = type.PPTY_MGMT_ AND info.HOUSE_STATUS = s.HOUSE_STAT AND info.AREA = A.Area_type ", function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results.length);
-    let len = results.length;
-    console.log(results[0].Ownership);
-    for(let i = 0; i < len; i++){
-        let os = results[i].Ownership;
-        if(os !== null && os !== ""){
-            results[i].Ownership = judgeOwnership(os);
-        }else{
-            results[i].Ownership = 0;
-        }
-    }
-    fs.writeFile("./res.json", JSON.stringify(results), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        let endTime = new Date();
-        let duration = parseInt((endTime-startTime)/minuteUnit)
-        console.log(duration + 'seconds');
-    });
+app.use(bodyParser.json());
+app.use(compression());
+
+app.use('/', express.static(__dirname + '/www'));
+app.get('/dataclean', data.dataClean.bind(this, connection));
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send(err);
 });
 
-connection.end();
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
-function judgeOwnership(owner){
-    let tmp = 0, type = 0;
-    let reg = /\d+/g;
-    //filter the occasion that '70(1992-9-23)年', '70（2006-10-16至2076-10-15）年'. we only wany the number 70
-    owner = owner.split('(')[0];
-    owner = owner.split('（')[0];
-    let res = owner.match(reg);
-    // console.log(res);
-    if(res === null) return type;
-    let arrLen = res.length;
-    if(arrLen === 0) return type;
-    let min = parseInt(res[0]);
-    if(arrLen === 1 ){
-        //filter the number that represent the year
-        if(min<100){
-            let n = parseInt(min);
-            return typeCalculator(n);
-        }else{
-            return type;
-        }
-    }
-    for(let i = 1; i < arrLen; i++){
-        let a = parseInt(res[i]);
-        if(a >= 100) continue;
-        if(a < min) min = a;
-    }
-    return typeCalculator(min);
-}
-
-function typeCalculator(num){
-    //type: 0: null,  1: <= 50,  2: >50 <= 70， 3: >70
-    //we set num should be bigger than 10.Because there are some data like 9号住宅楼:70，办公楼1-7号楼50年. We should do the filtering.
-    if(num <= 50 && num > 10){
-        return 1;
-    }else if(num > 50 && num <= 70){
-        return 2;
-    }else if(num > 70){
-        return 3;
-    }else{
-        return 0;
-    }
-}
+process.on('SIGINT', function() {
+    // todo sth
+    connection.end();
+    global.count = 0;
+    console.log("stop server");
+    process.exit(0);
+});
